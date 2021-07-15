adf5ac680934c56d0470944831f270246b62f76c
Yamilecen12 <centrinidad74@gmail.com>
2021-07-15 17:51:08 -0500

    Primer mensaje de la practica

---
+++ app/src/main/java/com/example/practica_almacenamiento/ExernoStorage.java
@@ -0,0 +1,14 @@
+package com.example.practica_almacenamiento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExernoStorage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exerno_storage);
+    }
+}
